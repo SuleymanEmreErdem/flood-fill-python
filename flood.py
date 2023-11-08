3a17b6bf515641a2e9e00fd8703a0ae383acd2e3
Süleyman Emre Erdem <121464020+SuleymanEmreErdem@users.noreply.github.com>
2023-11-08 21:19:25 +0300

    Add files via upload

---
+++ flood.py
@@ -0,0 +1,62 @@
+import pygame
+import numpy as np
+
+screen = pygame.display.set_mode((400, 400))
+
+maze_arr = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+                     [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+                     [1, 0, 1, 1, 0, 1, 0, 1, 0, 1], #1's are wall, 0's are road 
+                     [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+                     [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+                     [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+
+flood_arr = np.where(maze_arr==1, -1, maze_arr)
+
+def display_maze():
+    pygame.font.init()
+    myfont = pygame.font.SysFont('Arial', 30)
+    for i, x in enumerate(flood_arr):
+        for j, y in enumerate(x):
+            if y == -1:
+                pygame.draw.rect(screen, 'black', (j*40, i*40, 40, 40))
+            else:
+                rect = pygame.draw.rect(screen, 'white', (j*40, i*40, 40, 40))
+                textsurface = myfont.render(str(flood_arr[i][j]), False, (0, 0, 0))
+                screen.blit(textsurface, (rect.x+5, rect.y))
+
+def flood(x, y):
+    search = 1
+    flood_arr[x][y] = 1
+
+    while np.where(flood_arr == 0)[0].size != 0:
+        find = np.where(flood_arr == search)
+        for i, j in zip(*find):
+            if flood_arr[i-1][j] == 0:
+                flood_arr[i-1][j] = search + 1
+            if flood_arr[i+1][j] == 0:
+                flood_arr[i+1][j] = search + 1
+            if flood_arr[i][j-1] == 0:
+                flood_arr[i][j-1] = search + 1
+            if flood_arr[i][j+1] == 0:
+                flood_arr[i][j+1] = search + 1
+        search += 1
+
+
+if __name__ == '__main__':
+    print(maze_arr)
+    flood(4, 4) #parameter selects the start point
+    print(flood_arr)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
+        display_maze()
+
+        pygame.display.flip()
+
+    pygame.quit()
